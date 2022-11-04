a5e3cf94ed606d974d9284a2107b3010634414a5
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-11-04 13:48:27 -0600

    empezar

--- src/lab4p2_ingridhernandez_hectorhernandez/Lab4P2_IngridHernandez_HectorHernandez.java
+++ src/lab4p2_ingridhernandez_hectorhernandez/Lab4P2_IngridHernandez_HectorHernandez.java
@@ -4,6 +4,8 @@
  */
 package lab4p2_ingridhernandez_hectorhernandez;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMINISTRADOR1
@@ -14,7 +16,30 @@ public class Lab4P2_IngridHernandez_HectorHernandez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+Scanner lea = new Scanner(System.in);
+        
+        boolean centinela = true;
+        
+        while(centinela == true){
+            System.out.println("1)Ejercicio 1- ");
+             System.out.println("2)Salir");
+             System.out.print("Ingrese la opcion que desea: ");
+            int opcion = lea.nextInt();
+            switch (opcion){
+                case 1:{
+                    
+                }//fin del case 1
+                break;
+                
+                
+                case 2:
+                    centinela = false;
+                 break;
+           
+                default:
+           System.out.println("Opcion Incorrecta");
+            }//fin del switch
+    }//fin while
     }
     
 }
