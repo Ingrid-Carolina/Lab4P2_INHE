8b350f36f2c4f54f9102394cbedb441d42fb5059
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-11-04 15:28:46 -0600

    avance 8

--- src/lab4p2_ingridhernandez_hectorhernandez/Lab4P2_IngridHernandez_HectorHernandez.java
+++ src/lab4p2_ingridhernandez_hectorhernandez/Lab4P2_IngridHernandez_HectorHernandez.java
@@ -60,6 +60,126 @@ public class Lab4P2_IngridHernandez_HectorHernandez {
                             break;
                             case 2: {
 
+                                boolean centinela3 = true;
+                                while (centinela3 == true) {
+                                    System.out.println("============Tipo de Vehiculos ===========");
+                                    System.out.println("1)Aviones ");
+                                    System.out.println("2)Barcos ");
+                                    System.out.println("3)Submarinos");
+                                    System.out.println("4)Salir");
+                                    System.out.println("=========================================");
+                                    System.out.print("Ingrese la opcion que desea: ");
+                                    int o = lea.nextInt();
+                                    switch (o) {
+                                        case 1: {
+                                            int pos2;
+                                            pos2 = r.nextInt(vehiculos.length - 1);
+                                            String nombre = vehiculos[pos2];
+                                            int da = 100 + r.nextInt(150);
+                                            int cordX = 0;
+                                            int coordY = 0;
+                                            int vida = 500 + r.nextInt(500);
+                                            int pos;
+                                            pos = r.nextInt(jug.size()- 1);
+//                                String nom = jug.get(pos);
+                                            System.out.println("Tipo de Equipo: ");
+                                            System.out.println("1)Equipo Azul");
+                                            System.out.println("2)Equipo Rojo");
+                                            System.out.println("Ingrese el Tipo de Equipo que Desea: ");
+                                            int p = lea.nextInt();
+                                            String t = "";
+                                            if (p == 1) {
+                                                t = "Azul ";
+                                            } else if (p == 2) {
+                                                t = "Rojo";
+                                            } else {
+                                                System.out.println("El numero que Ingrese no Esta:");
+                                            }
+                                            System.out.println("Ingrese la Coordenada X:");
+                                            int corx = lea.nextInt();
+                                            System.out.println("Ingrese la Coordenada Y:");
+                                            int corY = lea.nextInt();
+                                            veh.add(new Aviones(corx, corY, nombre, da, cordX, coordY, vida, jug.get(pos), t));
+                                            System.out.println("Avion");
+                                            System.out.println(veh);
+                                        }//fin del case 1
+                                        break;
+                                        case 2: {
+                                            int pos2;
+                                            pos2 = r.nextInt(vehiculos.length - 1);
+                                            String nombre = vehiculos[pos2];
+                                            int da = 100 + r.nextInt(150);
+                                            int cordX = 0;
+                                            int coordY = 0;
+                                            int vida = 500 + r.nextInt(500);
+                                            int pos;
+                                            pos = r.nextInt(jug.size() - 1);
+//                                String nom = jug.get(pos);
+                                            System.out.println("Tipo de Equipo: ");
+                                            System.out.println("1)Equipo Azul");
+                                            System.out.println("2)Equipo Rojo");
+                                            System.out.println("Ingrese el Tipo de Equipo que Desea: ");
+                                            int p = lea.nextInt();
+                                            String t = "";
+                                            if (p == 1) {
+                                                t = "Azul ";
+                                            } else if (p == 2) {
+                                                t = "Rojo";
+                                            } else {
+                                                System.out.println("El numero que Ingrese no Esta:");
+                                            }
+                                            System.out.println("Ingrese la Coordenada X:");
+                                            int corx = lea.nextInt();
+                                            System.out.println("Ingrese la Coordenada Y:");
+                                            int corY = lea.nextInt();
+
+                                            veh.add(new Barcos(corx, corY, nombre, da, cordX, coordY, vida, jug.get(pos), t));
+                                            System.out.println("Barco");
+                                            System.out.println(veh);
+                                        }//fin del case 2
+                                        break;
+                                        case 3: {
+                                            int pos2;
+                                            pos2 = r.nextInt(vehiculos.length - 1);
+                                            String nombre = vehiculos[pos2];
+                                            int da = 100 + r.nextInt(150);
+                                            int cordX = 0;
+                                            int coordY = 0;
+                                            int vida = 500 + r.nextInt(500);
+                                            int pos;
+                                            pos = r.nextInt(jug.size() - 1);
+//                                String nom = jug.get(pos);
+                                            System.out.println("Tipo de Equipo: ");
+                                            System.out.println("1)Equipo Azul");
+                                            System.out.println("2)Equipo Rojo");
+                                            System.out.println("Ingrese el Tipo de Equipo que Desea: ");
+                                            int p = lea.nextInt();
+                                            String t = "";
+                                            if (p == 1) {
+                                                t = "Azul ";
+                                            } else if (p == 2) {
+                                                t = "Rojo";
+                                            } else {
+                                                System.out.println("El numero que Ingrese no Esta:");
+                                            }
+                                            System.out.println("Ingrese la Coordenada X:");
+                                            int corx = lea.nextInt();
+                                            System.out.println("Ingrese la Coordenada Y:");
+                                            int corY = lea.nextInt();
+                                            veh.add(new Submarinos(corx, corY, nombre, da, cordX, coordY, vida, jug.get(pos), t));
+                                            System.out.println("Submarino");
+                                            System.out.println(veh);
+                                        }//fin del case 3
+                                        break;
+                                        case 4:
+                                            centinela3 = false;
+                                            break;
+
+                                        default:
+                                            System.out.println("Opcion Incorrecta");
+                                    }//fin del switch
+                                }//fin while
+                               
                             }//fin del case 2
                             break;
 
